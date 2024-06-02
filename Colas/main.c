4c4b6868903b47ad4a15178224cd70da7ced5306
Anrago <141792891+Anrago@users.noreply.github.com>
2024-06-01 19:04:53 -0700

    assasa

--- Colas/main.c
+++ Colas/main.c
@@ -21,12 +21,20 @@ typedef struct _cola
 
 } Tcola;
 
+//Declaracion de funciones
 Tnodo *CreateNodo(Tval *datos);
+Tval Front(Tcola *cola);
 void DestroyNodo(Tnodo *nodo);
-void Push(Tcola *cola, Tval *datos);
-void Pop(Tcola *cola);
+void Enqueue(Tcola *cola, Tval *datos);
+void Dequeue(Tcola *cola);
+void IsEmpy(Tcola *cola);
+void Clear(Tcola *cola);
 int PrintMenu();
 
+
+
+
+// Funcion principal
 int main()
 {
     Tval *datos = (Tval *)malloc(sizeof(Tval));
@@ -36,28 +44,39 @@ int main()
     cola->cantCola = 0;
     int opc;
     do
-    {
+    {   
+        system("CLEAR");
         opc = PrintMenu();
-
+        system("CLEAR");
         switch (opc)
         {
         case 1:
             printf("INGRESA UN VALOR A LA COLA\n");
             scanf("%d", &datos->num);
-            Push(cola, datos);
+            Enqueue(cola, datos);
             break;
         case 2:
             printf("ELIMINANDO ELEMENTO DE LA COLA\n");
-            Pop(cola);
-            getchar();
+            Dequeue(cola);
             break;
         case 3:
-            printf("IMPRIMIENDO COLA\n");
-
+            printf("OBTENER ELEMENTO CABEZA\n");
+            Tval elemento=Front(cola);
+            printf("Elemento cabeza = %d\n",elemento);
+            system("PAUSE");
+            break;
+        case 4:
+            printf("IS EMPTY?\n");
+            IsEmpy(cola);
+            break;
+        case 5:
+            printf("VACIAR COLA\n");
+            Clear(cola);
             break;
         default:
             break;
         }
+        
     } while (opc != 0);
 }
 
@@ -77,7 +96,7 @@ void DestroyNodo(Tnodo *nodo)
     free(nodo);
 }
 
-void Push(Tcola *cola, Tval *datos)
+void Enqueue(Tcola *cola, Tval *datos)
 {
     Tnodo *nodo = CreateNodo(datos);
     Tnodo *temp = cola->cabeza;
@@ -98,18 +117,46 @@ void Push(Tcola *cola, Tval *datos)
     }
 }
 
-void Pop(Tcola *cola)
+void Dequeue(Tcola *cola)
 {
     Tnodo *temp;
-    printf("DATOS ELIMNADO DE LA LISTA");
+    printf("DATO ELIMNADO DE LA LISTA: ");
     if (cola->cabeza)
     {
         temp = cola->cabeza;
         cola->cabeza = cola->cabeza->siguiente;
         cola->cantCola--;
-        printf("%d", temp->datos);
+        printf("%d \n", temp->datos);
+        DestroyNodo(temp);
+        system("PAUSE");
+    }
+}
+
+Tval Front(Tcola *cola)
+{
+    return cola->cabeza->datos;
+}
+
+void IsEmpy(Tcola *cola)
+{
+    if(cola->cabeza != NULL)
+        printf("LA COLA NO ESTA VACIA\n");
+    else
+        printf("LA COLA ESTA VACIA\n");
+    system("PAUSE");
+}
+
+void Clear(Tcola *cola)
+{
+    while (cola->cabeza != NULL)
+    {
+        Tnodo * temp = cola->cabeza;
+        cola->cabeza=cola->cabeza->siguiente;
         DestroyNodo(temp);
     }
+    cola->Ultimo = NULL;
+    printf("COLA VACIADA CON EXITO \n");
+    system("PAUSE");
 }
 
 int PrintMenu()
@@ -118,7 +165,9 @@ int PrintMenu()
     printf("MENU\n");
     printf("1.-Agregar\n");
     printf("2.-Eliminar\n");
-    printf("3.-Imprimir\n");
+    printf("3.-Obtener elemento cabeza\n");
+    printf("4.-IsEmpty\n");
+    printf("5.-Vaciar cola\n");
     printf("0.-Salir\n");
     printf("Elije una opcion: ");
     scanf("%d", &opc);
